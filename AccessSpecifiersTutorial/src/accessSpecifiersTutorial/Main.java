705af9cbc3eeba2a9981ea9185475769235c0328
VinuthCS <vinuthcs90@yahoo.com>
2020-02-12 10:29:21 -0500

    updated Main Class to demonstrate the use of access specifiers

--- AccessSpecifiersTutorial/src/accessSpecifiersTutorial/Main.java
+++ AccessSpecifiersTutorial/src/accessSpecifiersTutorial/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Creating an instance of PrivateClassExample
-		PrivateClassExample privateClassExample = new PrivateClassExample();
-		
 		System.out.println("Trying to access the data member and method "
 				+ "of PrivateClassExample");
 		
@@ -15,22 +12,20 @@ public class Main {
 		 * and can not be accessed outside the PrivateClassExample
 		 ***/
 		
-		//System.out.println(privateMethod.privateString);
-		//privateClassExample.privateMethod();
-		
-		//Creating an instance of PrivateClassExample
-		ProtectedClassExample protectedClassExample = new ProtectedClassExample();
+		//System.out.println(PrivateClassExample.privateString);
+		//PrivateClassExample.privateMethod();
 				
 		System.out.println("Trying to access the data member and method "
 						+ "of ProtectedClassExample");
-				
-		/***
-		 * Below lines will throw error because the member and method are private
-		 * and can not be accessed outside the PrivateClassExample
-		 ***/
-				
-		System.out.println(protectedClassExample.protectedString);
-		protectedClassExample.protectedMethod();
+		ProtectedClassExample.protectedMethod();
+		
+		System.out.println("Trying to access the data member and method "
+				+ "of ProtectedClassExample by using ProtectedChildClass");
+		ProtectedChildClass.protectedChildMethod();
+		
+		System.out.println("Trying to access the data member and method "
+				+ "of PublicClassExample");
+		PublicClassExample.publicMethod();
 		
 		
 

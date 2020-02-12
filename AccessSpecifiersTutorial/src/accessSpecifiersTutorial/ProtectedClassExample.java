102081ed0460bab090b00b21b2ccb529a574f097
VinuthCS <vinuthcs90@yahoo.com>
2020-02-12 09:53:05 -0500

    Added ProtectedClassExample class to the project to demonstrate the scope of protected data member and method

---
+++ AccessSpecifiersTutorial/src/accessSpecifiersTutorial/ProtectedClassExample.java
@@ -0,0 +1,22 @@
+package accessSpecifiersTutorial;
+
+public class ProtectedClassExample {
+	
+	/***
+	 * This class is used to demonstrate the scope of protected
+	 * data member and method
+	***/
+	
+	//Creating a protected data member of type String
+	protected String protectedString = "Protected Member of ProtectedClassExample";
+	
+	//Creating a protected method to access the data member
+	protected void protectedMethod() {
+		
+		System.out.println("This is a protected method in ProtectedClassExample "
+				+ "trying to access the protected data member");
+		System.out.println(protectedString);
+		
+	}
+
+}

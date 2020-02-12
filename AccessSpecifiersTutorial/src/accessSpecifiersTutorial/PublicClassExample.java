f4f412f8447272fe13c043848f2dbb608968ec66
VinuthCS <vinuthcs90@yahoo.com>
2020-02-12 10:01:22 -0500

    Added PublicClassExample class to the project to demonstrate the scope of public data member and method

---
+++ AccessSpecifiersTutorial/src/accessSpecifiersTutorial/PublicClassExample.java
@@ -0,0 +1,22 @@
+package accessSpecifiersTutorial;
+
+public class PublicClassExample {
+	
+	/***
+	 * This class is used to demonstrate the scope of public
+	 * data member and method
+	***/
+	
+	//Creating a private data member of type String
+	public String publicString = "Public Member of PublicClassExample";
+	
+	//Creating a public method to access the data member
+	public void publicMethod() {
+		
+		System.out.println("This is a public method in PublicClassExample "
+				+ "trying to access the public data member");
+		System.out.println(publicString);
+		
+	}
+
+}
